77c80d184ea654f79991a40127bd513b5981cdef
Sơn Nguyễn <son@sonng.dev>
2023-01-22 14:13:06 +0000

    feat: Update year number in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -43,7 +43,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>{`${new Date().getFullYear()} • ${config.author}`}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
